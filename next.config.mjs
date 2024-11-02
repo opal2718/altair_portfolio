5fc0f319bfeb6a6fd1275300a918eb20afdb7d4c
opal2718 <littlesihoo@gmail.com>
2024-11-02 23:41:12 +0900

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+//const nextConfig = {};
+const nextConfig = {
+    basePath: "/altair_portfolio",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
 export default nextConfig;
